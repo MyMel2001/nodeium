d70cd63286a4a06b3b394e97952cbb8bd951c1dc
sparky <no@nope.nicetry>
2025-10-27 20:25:18 +0000

    Update main.js

--- main.js
+++ main.js
@@ -172,7 +172,7 @@ const regexPatterns = [
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   let x = createWindow()
-  enableGoodies().then()
+  enableGoodies(session.defaultSession).then()
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
